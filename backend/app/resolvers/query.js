f61820e57ea9033038215f272fa34abdf5677ae9
ceciliahaentjens <cecilia.haentjens@gmail.com>
2022-04-25 14:40:01 +0200

    Remove unnecessary example queries

--- backend/app/resolvers/query.js
+++ backend/app/resolvers/query.js
@@ -1,12 +1,8 @@
 module.exports = {
-    hello() {
-        return {
-            world: 'Hello World',
-        };
-    },
 
     async getAllPointsOfSale(_, args, { dataSources }) {
         const data = await dataSources.pointOfSale.findAll();
         return data;
     },
+
 };
