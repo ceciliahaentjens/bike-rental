46f016904d2ccc29d4984e69734ad2bec4e10eec
ceciliahaentjens <cecilia.haentjens@gmail.com>
2022-04-25 11:44:26 +0200

    Back-end initialization

--- backend/data/init.sql
+++ backend/data/init.sql
@@ -1,6 +1,6 @@
 -- Si database already exist
 DROP DATABASE IF EXISTS ovelo;
-DROP ROLE IF EXISTS ovelo;
+DROP ROLE IF EXISTS ovelo_u;
 
 CREATE ROLE ovelo_u WITH LOGIN PASSWORD 'ovelo';
 CREATE DATABASE ovelo OWNER ovelo_u;
