26d76639d9baa70d011aeaf6ed2e793700af7a4e
ceciliahaentjens <cecilia.haentjens@gmail.com>
2022-04-26 15:24:28 +0200

    GetAllKindsOfBike query

--- backend/app/resolvers/query.js
+++ backend/app/resolvers/query.js
@@ -5,6 +5,11 @@ module.exports = {
         return data;
     },
 
+    async getAllKindsOfBike(_, args, { dataSources }) {
+        const data = await dataSources.kindOfBike.findAll();
+        return data;
+    },
+
     async getAllBikes(_, args, { dataSources }) {
         const data = await dataSources.bike.findAll();
         return data;
