4d472aa70995b7431e19bf512783f6769ebf19af
ceciliahaentjens <cecilia.haentjens@gmail.com>
2022-04-26 17:51:41 +0200

    WIP - Filter the Bikes list

--- frontend/src/components/BikesList/index.tsx
+++ frontend/src/components/BikesList/index.tsx
@@ -15,9 +15,41 @@ import { GET_ALL_POINTS_OF_SALE } from '../../apollo/queries/allPointsOfSale';
 function BikesList() {
 
     // Je récupère les données de ma query
+    const { data: bikesData } = useQuery<getAllBikes>(GET_ALL_BIKES, {
+        variables: {
+            take: 5
+        }
+    });
     const { data: kindsOfBikeData } = useQuery<getAllKindsOfBike>(GET_ALL_KINDS_OF_BIKE);
-    const { data: bikesData } = useQuery<getAllBikes>(GET_ALL_BIKES);
     const { data: pointsOfSaleData } = useQuery<GetAllPointsOfSale>(GET_ALL_POINTS_OF_SALE);
+
+    const [type, setType] = useState('');
+    const [status, setStatus] = useState('');
+    const [pointOfSale, setPointOfSale] = useState('');
+    const [bikesToDisplay, setBikesToDisplay] = useState(() => {
+        return bikesData ? bikesData.getAllBikes : [];
+    });
+
+    function filterBikesList(type: String, status: String, pointOfSale: String) {
+        let filtered = bikesData ? bikesData.getAllBikes : [];
+
+        if (type) {
+            filtered = filtered?.filter((bike) => 
+                bike.kind.id === Number(type)
+            );
+        }
+        if (status) {
+            filtered = filtered?.filter((bike) => 
+                (bike.status).toUpperCase() === status.toUpperCase()
+            );
+        }
+        if (pointOfSale) {
+            filtered = filtered?.filter((bike) => 
+                bike.pointOfSale.id === Number(pointOfSale)
+            );
+        }       
+        setBikesToDisplay(filtered);
+    }
 
     return (
         <Container>
@@ -29,6 +61,11 @@ function BikesList() {
                 alignItems="center"
                 spacing={3}
                 sx={{ mb: 6 }}
+                component="form"
+                onSubmit={(event: React.SyntheticEvent) => {
+                    event.preventDefault();
+                    filterBikesList(type, status, pointOfSale);
+                }}
             >
                 <FormControl sx={{ m: 1, minWidth: 180 }} size="small">
                     <InputLabel id="type-select-label">Type</InputLabel>
@@ -36,6 +73,10 @@ function BikesList() {
                         labelId="type-select-label"
                         id="type-select"
                         label="Type"
+                        value={type}
+                        onChange={(event) => {
+                            setType(event.target.value);
+                        }}
                     >
                         {
                             kindsOfBikeData && kindsOfBikeData.getAllKindsOfBike.map((kindOfBike: getAllKindsOfBike_getAllKindsOfBike) => (
@@ -50,6 +91,10 @@ function BikesList() {
                         labelId="status-select-label"
                         id="status-select"
                         label="Statut"
+                        value={status}
+                        onChange={(event) => {
+                            setStatus(event.target.value);
+                        }}
                     >
                         <MenuItem key="available" value="available">Disponible</MenuItem>
                         <MenuItem key="rent" value="rent">Loué</MenuItem>
@@ -63,6 +108,10 @@ function BikesList() {
                         labelId="point-of-sale-select-label"
                         id="point-of-sale-select"
                         label="Point de vente"
+                        value={pointOfSale}
+                        onChange={(event) => {
+                            setPointOfSale(event.target.value);
+                        }}
                     >
                         {
                             pointsOfSaleData && pointsOfSaleData.getAllPointsOfSale.map((pointOfSale: GetAllPointsOfSale_getAllPointsOfSale) => (
@@ -71,7 +120,7 @@ function BikesList() {
                         }
                     </Select>
                 </FormControl>
-                <Button variant="contained">Filtrer</Button>
+                <Button type="submit" variant="contained">Filtrer</Button>
             </Stack>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -88,7 +137,7 @@ function BikesList() {
                     </TableHead>
                     <TableBody>
                         {
-                            bikesData && bikesData.getAllBikes.map((bike: getAllBikes_getAllBikes) => (
+                            bikesData && bikesToDisplay.map((bike: getAllBikes_getAllBikes) => (
                                 <TableRow
                                     key={bike.id}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
