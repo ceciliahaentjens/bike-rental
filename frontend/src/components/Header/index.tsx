3f8a763123e989412c452c800734e0329894e2e5
ceciliahaentjens <cecilia.haentjens@gmail.com>
2022-04-26 10:21:43 +0200

    Add stored point of sale in header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,20 +1,32 @@
 import { Link } from 'react-router-dom';
 // Mui
 import {
-    AppBar, Toolbar, Button,
+    AppBar, Toolbar, Button, Typography, Box
 } from '@mui/material';
 
 function Header() {
+    const pointOfSale = localStorage.getItem('stored-point-of-sale');
+
     return (
         <AppBar component="header" position="static" sx={{ mb: 4 }}>
             <Toolbar sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between' }}>
-                <Button color="inherit">VélO'</Button>
-                <div>
-                    <Button color="inherit">Accueil</Button>
+                <Button color="inherit" component={Link} to='/'>VélO'</Button>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}
+                >
+                    {
+                        pointOfSale !== null && (
+                            <Typography color="warning" sx={{ mr: 2 }}>Point de vente actuel&nbsp;: {JSON.parse(pointOfSale).label}</Typography>
+                        )
+                    }
+                    <Button color="inherit" component={Link} to='/'>Accueil</Button>
                     <Button color="inherit">Liste de vélos</Button>
                     <Button color="inherit">Démarrer une location</Button>
                     <Button color="inherit">Terminer une location</Button>
-                </div>
+                </Box>
             </Toolbar>
         </AppBar>
     )
