967e69569720bed5a0f7e7eceb3e7d8f72a0058d
ceciliahaentjens <cecilia.haentjens@gmail.com>
2022-04-26 10:07:24 +0200

    Check if the pointOfSale in localStorage is expired

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,29 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import PointsOfSaleList from './components/PointsOfSaleList';
 
+function checkIfPointOfSaleIsExpired() {
+  const storedPointOfSaleTimestamp = localStorage.getItem('stored-point-of-sale-expire');
+  
+  // Check if the key exists in localStorage
+  if (storedPointOfSaleTimestamp === null) { return; }
+
+  const then = new Date(JSON.parse(storedPointOfSaleTimestamp));
+  const now = new Date();
+
+  // Je calcule la différence entre les deux dates (now & then)
+  const msBetweenDates = Math.abs(then.getTime() - now.getTime());
+  const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
+
+  // Si la date est + 24 heures
+  if (hoursBetweenDates > 24) {
+    // Je supprime les éléments du localStorage
+    localStorage.removeItem('stored-point-of-sale')
+    localStorage.removeItem('stored-point-of-sale-expire')
+  }
+}
+
 function App() {
+  checkIfPointOfSaleIsExpired();
   return (
     <React.Fragment>
       <CssBaseline />
