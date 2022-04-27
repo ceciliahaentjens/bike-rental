7dfd733eb557dbad19d1624a84345fdf6421879d
ceciliahaentjens <cecilia.haentjens@gmail.com>
2022-04-27 10:22:58 +0200

    Create a rent on BE

--- backend/app/resolvers/mutation.js
+++ backend/app/resolvers/mutation.js
@@ -1,5 +1,44 @@
+const { UserInputError } = require('apollo-server');
+
 module.exports = {
     exampleHello(_, args) {
         return args.input;
+    },
+
+    async createRent(_, args, { dataSources }) {
+        const data = args.input;
+        const bike = await dataSources.bike.findByPk(data.bike_id);
+
+        // On vérifie si le vélo existe bien
+        if (!bike) {
+            throw new UserInputError('The selected bike doesn\'t exist');
+        }
+        // On vérifie si le vélo est disponible
+        if (bike.status !== 'AVAILABLE') {
+            throw new UserInputError('The selected bike isn\'t available');
+        }
+        // On vérifie si il existe au point de vente sélectionné
+        if (bike.point_of_sale_id !== data.rent_point_of_sale_id) {
+            throw new UserInputError('The selected bike isn\'t available at the current point of sale');
+        }
+
+        // J'ajoute la date de début à la mutation
+        const startDate = new Date().toISOString();
+        data.start_date = startDate;
+
+        // Je crée la location
+        const newRent = await dataSources.rent.insert(data);
+
+        // Je marque le vélo comme étant loué
+        await dataSources.bike.update({ id: bike.id }, { status: 'RENT' });
+
+        // J'ajoute le vélo à l'objet retourné
+        newRent.bike = bike;
+
+        // J'ajoute le point de vente à l'objet retourné
+        const pointOfSale = await dataSources.pointOfSale.findByPk(data.rent_point_of_sale_id);
+        newRent.rent_point_of_sale = pointOfSale;
+
+        return newRent;
     },
 };
